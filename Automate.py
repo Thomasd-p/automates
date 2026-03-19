52f6685bdd51ce1b3c98c4bd5293f92ba4cf254b
Fekoua Motcheka Karl Junior <juniorfekoua@gmail.com>
2026-03-19 19:17:13 +0100

    Refactor state initialization in read_txt method
    Refactor read_txt method to improve readability and organization of state initialization.

--- Automate.py
+++ Automate.py
@@ -5,37 +5,59 @@ class Automate:
     def read_txt(self,file):
         with open(file, "r") as file :
             automate = file.readlines()
-            self.num_symbols = int(automate[0].strip()) # initialisation du nombre de symbole
-            self.num_states = int(automate[1].strip()) # initialisation du nombre d'etats
 
+            # initialisation du nombre de symbole de l'alphabet
+            self.num_symbols = int(automate[0].strip())
+
+            # initialisation du nombre d'etats
+            self.num_states = int(automate[1].strip()) 
+
+            # etat initiaux
             initial_line = automate[2].strip().split()
-            self.num_initial_states = int(initial_line[0]) #initialisation du
-            self.initial_states = list(map(int,initial_line[1:]))
+            self.num_initial_states = int(initial_line[0]) # nombre d'etats initiaux
+            self.initial_states = list(map(int,initial_line[1:])) # liste des etats initiaux
 
-            final_line = automate[3].strip().split()
-            self.num_final_states = final_line[0]
-            self.final_states = list(map(int,final_line[1:]))
+            # etat finaux
+            final_line = automate[3].strip().split() 
+            self.num_final_states = final_line[0] # nombre d'etats finaux
+            self.final_states = list(map(int,final_line[1:]))# liste des etats finaux
 
-
+            # initialisation du numbre de transition
             self.num_transitions = int(automate[4].strip())
+
+            # transitions (c'est une bibliothéque)
+            # clé = (état de depart, symbole) = liste d'etats d'arrivée
             self.transitions = {}
+
             if self.num_transitions != 0 :
                 for i in range(5, 5 + self.num_transitions):
                     line = automate[i].strip()
-
+                    
+                    # on cherche où s'arrête l'etat de depart
                     j= 0
                     while line[j].isdigit() :
                         j += 1
-
+                    
+                    # extration de l'etat de depart
                     debut = int(line[:j])
+
+                    # extraction du symbole de transition
                     altphabet = line[j]
+
+                    # extraction de l'etat d'arrivée
                     fin = int(line[j+1:])
 
+                    # creation de la clé (etat de depart, symbole)
                     key = (debut, altphabet)
+
+                    # si la clé n'existe pas, on initialse
                     if key not in self.transitions.keys():
                         self.transitions[key] = []
-
+                    
+                    # ajout de l'etat d'arrivée à la lite des transitions
                     self.transitions[key].append(fin)
+
+
 
     def display():
         return
